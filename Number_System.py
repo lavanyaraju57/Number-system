416ea8f66aba0cb168582b54fcc4c89a813799c3
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-12 20:01:28 +0530

    Add files via upload

---
+++ Number_System.py
@@ -0,0 +1,145 @@
+# from decimal import Decimal as d
+
+# h=d(0.9+0.5)
+# h=d(0.9)+d(0.5)
+# h=d('0.9')+d('0.5')
+# print(h)
+
+# a=0.5 ; b=0.2
+# c=a+b
+# print(c)
+# print("%.1f" %c)
+# r=str(c)
+# print(len(r))
+# print("%.1f" %c)
+
+# c=bin(4)
+# print(c,type(c))
+
+# h=0b101
+# print(h)
+# n=0B101
+# print(n)
+# print(0b100)
+# print(h,type(h))
+# print(n,type(n))
+
+# m=oct(15)
+# print(m,type(m))
+
+# c=0o25
+# print(c)
+# d=0O25
+# print(d)
+# print(c,type(c))
+# print(d,type(d))
+
+# x=hex(20)
+# print(x,type(x))
+
+# s=0xed
+# t=0Xed
+# print(s,type(s))
+# print(t,type(t))
+
+# print(hex(10))
+# print(0x15)
+# print(0X15)
+
+# print(5/10)
+
+# from fractions import Fraction as f
+# from re import S
+
+# a=f(5,0)
+# print(a)
+# a=f(2,2)
+# print(a)
+# a=(5,3)
+# print(a)
+# b=f(5)
+# print(b)
+# d=f(0,7)
+# print(d)
+# s=f(5,25)
+# print(s)
+
+# a=5_25
+# print(a,type(a))
+# s=5_3.0
+# print(s,type(s))
+
+# a=7;m=5.5
+# print(a,type(a))
+# print(m,type(m))
+# print(isinstance(a,int))
+# print(isinstance(a,float))
+# print(isinstance(m,bool))
+# print(isinstance(m,float))
+
+# m=5j
+# print(m,type(m))
+# s=5+7j
+# print(s,type(s))
+# print(isinstance(m,complex))
+
+# s=0+0j
+# print(s)
+
+# m=(5-8j)
+# print(m)
+
+# f=(0-8j)
+# print(f)
+
+# t=(0+(-5j))
+# print(t)
+
+
+# from fractions import Fraction as F
+
+# print(F(5.5))
+# print(F(7,9))
+# print(F('5.5'))
+# print(F('7.9'))
+# s=F(5.8)
+# print(s,type(s))
+# t=F(5,25)
+# print(t,type(t))
+
+# h=5+9j
+# print(h,type(h))
+# print(h.real)
+# print(h.imag)
+
+# g=5*4.9j
+# print(g,type(g))
+# print(g.real)
+# print(g.imag)
+
+# print(8)
+# print('8')
+# print("8")
+# print(+8)
+# print(+0.9)
+# print(+6+4j)
+# print(-5+8j)
+# print(0-0j)
+
+# print(1e0)
+# print(4e0)
+# print(6e1)
+# print(9e2)
+# print(12e2)
+# print(1.5e0)
+# print(1.5e2)
+# print(1.7e0)
+
+# print(4*5)
+# print(7**5)
+# print(9**2)
+# print(5.2*7)
+# print(7**3)
+
+
+
